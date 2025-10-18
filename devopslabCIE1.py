58f9487c7c4803b43e4f1a333d7d2678827f1d98
anusankanal <anusankanal@gmail.com>
2025-10-18 11:59:17 +0530

    Update devopslabCIE1.py

--- devopslabCIE1.py
+++ devopslabCIE1.py
@@ -1,12 +1,9 @@
-# Python program to classify temperature in Celsius and Fahrenheit
 
-# Get temperature input from user in Celsius
+
 celsius = float(input("Enter temperature in Celsius: "))
 
-# Convert Celsius to Fahrenheit
 fahrenheit = (celsius * 9/5) + 32
 
-# Classify temperature based on Celsius
 if celsius < 15:
     category = "Cold"
 elif 15 <= celsius <= 30:
@@ -14,5 +11,4 @@ elif 15 <= celsius <= 30:
 else:
     category = "Hot"
 
-# Display results
 print(f"Temperature: {celsius}°C / {fahrenheit:.2f}°F — It's {category}.")
